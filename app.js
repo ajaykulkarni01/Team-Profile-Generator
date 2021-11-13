3b2f8c1b46829c5bd5bfb0cf346ddefa49420bfd
Ajay K <ajaycoolkarni@yahoo.com>
2021-11-13 14:20:48 +1030

    questions set updated

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const Employee = require("./lib/Employee");
 let team = [];
 let canAddManager = true;
 
-// Write code to use inquirer to gather information about the development team members, and to create objects for each team member (using the correct classes as blueprints!)
+// to gather information about the development team members, and to create objects for each team member
 const questions = {
     Manager: [
         {
@@ -253,12 +253,7 @@ function addNewMember() {
 
 addNewMember();
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
+
 
 function generate() {
     fs.writeFileSync(outputPath, render(team), "utf-8");
